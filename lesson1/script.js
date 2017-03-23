2846ad3d4aae76a88fb86ca18866c269abba4ce5
KozlovaMS <m987ns197rus@gmail.com>
2017-03-23 10:47:46 +0300

    Домашняя работа к первому уроку

---
+++ lesson1/script.js
@@ -0,0 +1,88 @@
+﻿function Container()
+{
+    this.id = "";
+    this.className = "";
+    this.htmlCode = "";
+    
+    this.render = function()
+    {
+        return this.htmlCode;
+    }
+    
+    this.remove = function()
+    {
+        this.id = "";
+        this.className = "";
+        this.htmlCode = "";
+        return true;
+    }
+}
+
+function Menu(elId, elClass, items)
+{
+    Container.call(this);
+    this.id = elId;
+    this.className = elClass;
+    this.items = items;
+    
+    this.render = function()
+    {
+        var result = '<ul class="' + this.className + '" id="' + this.id + '">';
+        result += items.map(function(item){
+            return item.render();
+        }).join('');
+        result += '</ul>';
+        return result;
+    }
+}
+
+function MenuItem(elHref, elName)
+{
+    Container.call(this);
+    this.href = elHref;
+    this.name = elName;
+    
+    this.render = function()
+    {
+        return '<li class="' + this.className + '"><a href="' + this.href + '">' + this.name + '</a></li>';
+    }
+}
+
+/*
+
+//Мне кажется, что для построения всего меню достаточно одного класса
+//Класс NewMenu принимает в качестве параметра item массив объектов такого вида:
+var items = [
+    {href: '/', name: 'Главная'},
+    {href: '/gal', name: 'Галерея'},
+    {href: '/cat', name: 'Каталог', subitems: [
+        {href: '/cat/category1', name: 'Категория 1'},
+        {href: '/cat/category2', name: 'Категория 2'},
+        {href: '/cat/category3', name: 'Категория 3'}
+    ]}
+];
+
+*/
+
+function NewMenu(elClass, elChildClass, items)
+{
+    Container.call(this);
+    this.className = elClass;
+    this.classChildName = elChildClass;
+    this.items = items;
+    
+    this.render = function()
+    {
+        var result = '<ul class="' + this.className + '">';
+        result += items.map(function(item){
+            if ('subitem' in item){
+                var submenu = new NewMenu(elClass, elChildClass, item.subitem);
+                return '<li class="' + this.classChildName + '"><a href="' + item.href + '">' + item.name + '</a>' + submenu.render() + '</li>';
+            } else {
+                return '<li class="' + this.classChildName + '"><a href="' + item.href + '">' + item.name + '</a></li>';
+            }
+        }).join('');
+        result += '</ul>';
+        return result;
+    }
+}
---
+++ lesson1/script2.js
@@ -0,0 +1,139 @@
+﻿function Hamburger(size = "", stuffing = [], topping = [])
+{
+    const SIZE_SMALL_PRICE = 50;
+    const SIZE_SMALL_CALORIE = 20;
+    const SIZE_LARGE_PRICE = 100;
+    const SIZE_LARGE_CALORIE = 40;
+    const STUFFING_CHEESE_PRICE = 10;
+    const STUFFING_CHEESE_CALORIE = 20;
+    const STUFFING_SALAD_PRICE = 20;
+    const STUFFING_SALAD_CALORIE = 5;
+    const STUFFING_POTATO_PRICE = 15;
+    const STUFFING_POTATO_CALORIE = 10;
+    const TOPPING_MAYO_PRICE = 20;
+    const TOPPING_MAYO_CALORIE = 5;
+    const TOPPING_SPICE_PRICE = 15;
+    const TOPPING_SPICE_CALORIE = 0;
+    
+    this.size = size;
+    this.stuffing = stuffing;
+    this.topping = topping;
+    this.price = "";
+    this.calorie = "";
+    this.description = "";
+    this.error = false;
+    this.message = {};
+    
+    this.calcPrice = function()
+    {
+        var result = 0;
+        //добавляем размер гамбургера
+        if (this.size == 'small'){
+            result += SIZE_SMALL_PRICE;
+        } else if (this.size == 'large'){
+            result += SIZE_LARGE_PRICE;
+        } else {
+            this.error = true;
+            this.message['size'] = 'Пожалуйста, выберите размер гамбургера!';
+        }
+        //добавляем выбранные начинки
+        if (this.stuffing.length > 0){
+            for (var i in this.stuffing){
+                if (i == 'cheese'){
+                    result += STUFFING_CHEESE_PRICE;
+                } else if (i == 'salad'){
+                    result += STUFFING_SALAD_PRICE;
+                } else if (i == 'potato'){
+                    result += STUFFING_POTATO_PRICE;
+                } else {
+                    this.message['otherStuffing'] = 'Вы указали неизвестную начинку: ' + i + '!';
+                }
+            }
+        } else {
+            this.error = true;
+            this.message['stuffing'] = 'Пожалуйста, выберите начинку для гамбургера!';
+        }
+        //добавляем выбранные топпинги
+        if (this.topping.length > 0){
+            for (var i in this.topping){
+                if (i == 'mayo'){
+                    result += TOPPING_MAYO_PRICE;
+                } else if (i == 'spice'){
+                    result += TOPPING_SPICE_PRICE;
+                } else {
+                    this.message['otherTopping'] = 'Вы указали неизвестный топпинг: ' + i + '!';
+                }
+            }
+        }
+        return result;
+    }
+    
+    this.calcCalorie = function()
+    {
+        var result = 0;
+        //добавляем размер гамбургера
+        if (this.size == 'маленький'){
+            result += SIZE_SMALL_CALORIE;
+        } else if (this.size == 'большой'){
+            result += SIZE_LARGE_CALORIE;
+        } else {
+            this.error = true;
+            this.message['size'] = 'Пожалуйста, выберите размер гамбургера!';
+        }
+        //добавляем выбранные начинки
+        if (this.stuffing.length > 0){
+            for (var i in this.stuffing){
+                if (i == 'сыр'){
+                    result += STUFFING_CHEESE_CALORIE;
+                } else if (i == 'салат'){
+                    result += STUFFING_SALAD_CALORIE;
+                } else if (i == 'картофель'){
+                    result += STUFFING_POTATO_CALORIE;
+                } else {
+                    this.message['otherStuffing'] = 'Вы указали неизвестную начинку: ' + i + '!';
+                }
+            }
+        } else {
+            this.error = true;
+            this.message['stuffing'] = 'Пожалуйста, выберите начинку для гамбургера!';
+        }
+        //добавляем выбранные топпинги
+        if (this.topping.length > 0){
+            for (var i in this.topping){
+                if (i == 'майонез'){
+                    result += TOPPING_MAYO_CALORIE;
+                } else if (i == 'приправа'){
+                    result += TOPPING_SPICE_CALORIE;
+                } else {
+                    this.message['otherTopping'] = 'Вы указали неизвестный топпинг: ' + i + '!';
+                }
+            }
+        }
+        return result;
+    }
+    
+    this.descAdd = function()
+    {
+        return '<p>Размер гамбургера: ' + this.size + '</p><p>Выбранные начинки: ' + this.stuffing.join(', ') + '</p><p>Выбранные топпинги: ' + this.topping.join(', ') + '</p>';
+        return result;
+    }
+    
+    this.render = function()
+    {
+        var result = ""
+        this.price = this.calcPrice();
+        this.calorie = this.calcCalorie();
+        if (this.error){
+            for (var i in this.message){
+                result += '<p style="color: red;">' + this.message[i] + '</p>';
+            }
+        } else {
+            this.description = this.descAdd();
+            result += '<p>Цена гамбургера: ' + this.price + ' руб.</p><p>Калорийность гамбургера: ' + this.calorie + ' калорий.</p>' + this.description;
+            for (var i in this.message){
+                result += '<p style="color: red;">' + this.message[i] + '</p>';
+            }
+        }
+        return result;
+    }
+}
